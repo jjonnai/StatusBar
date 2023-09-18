982704d3397aa750ba699094696c9e946d4c8744
Jonna Huotari <t1hujo04@students.oamk.fi>
2023-09-18 17:12:56 +0300

    Valmis

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView} from 'react-native';
+import Constants from 'expo-constants'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text>StatusBar tehtävä</Text>
+      <StatusBar style= "auto"
+      backgroundColor='red'
+     // hidden={true}
+     />
+    </SafeAreaView>
+  )
+
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container:{
+    flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
+
 });
